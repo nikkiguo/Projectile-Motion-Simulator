18ea288dcafe8829365dc11b9f6ab0d2b229e1c3
Nikki <46271636+nikkiguo@users.noreply.github.com>
2020-05-10 21:34:25 -0400

    Initial commit

---
+++ NikkiGuo_Culminating.java
@@ -0,0 +1,22 @@
+// The "NikkiGuo_Culminating" class.
+import java.awt.*;
+
+public class NikkiGuo_Culminating extends Frame
+{
+    public NikkiGuo_Culminating ()
+    {
+	super ("NikkiGuo_Culminating"); // Set the frame's name
+	setSize (400, 400);     // Set the frame's size
+	show ();                // Show the frame
+    } // Constructor
+    
+    public void paint (Graphics g)
+    {
+	// Place the drawing code here
+    } // paint method
+    
+    public static void main (String[] args)
+    {
+	new NikkiGuo_Culminating ();    // Create a NikkiGuo_Culminating frame
+    } // main method
+} // NikkiGuo_Culminating class
