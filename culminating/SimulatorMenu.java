e223b2077b537a9077649f77f0f8b2ad43d1f463
Nikki <46271636+nikkiguo@users.noreply.github.com>
2020-05-13 15:56:30 -0400

    Add buttons

--- culminating/SimulatorMenu.java
+++ culminating/SimulatorMenu.java
@@ -9,15 +9,15 @@ import java.awt.event.ActionListener;
 
 public class SimulatorMenu extends JFrame
 {
-    final static Font TITLE_FONT = new Font ("Courier New", Font.PLAIN, 30);
+    final static Font TITLE_FONT = new Font ("Courier New", Font.PLAIN, 40);
     private JLabel title;
     private JPanel contentPanel;
     static JFrame frame;
-    static Color bermuda = new Color (145, 212, 194);
+    static Color bermuda = new Color (137, 171, 227);
 
     // Create menu JButtons in array
     private JButton[] menuButtons = {
-	new JButton ("B1"),
+	new JButton ("B"),
 	new JButton ("B2"),
 	new JButton ("B3")
 	};
@@ -28,18 +28,23 @@ public class SimulatorMenu extends JFrame
 	frame = new JFrame ("Projectile Motion Simulator");
 
 	// Create JLabel title
-	title = new JLabel ("Projectiles In Motion", JLabel.CENTER);
+	title = new JLabel ("- Projectile Motion -", JLabel.CENTER);
 	title.setFont (TITLE_FONT);
+	title.setForeground(Color.white);
 
 	contentPanel = new JPanel ();
 	contentPanel.setBorder (new EmptyBorder (new Insets (50, 50, 50, 50)));
-	contentPanel.setLayout (new GridLayout (5, 1, 0, 10));
+	contentPanel.setLayout (new GridLayout (5, 2, 0, 10));
 	contentPanel.setBackground (bermuda);
 	contentPanel.add (title);
+       
+	for (int i = 0; i < 3; i++) {
+	    contentPanel.add(menuButtons[i]);
+	}
 
 	// Customize frame and show it
 	frame.getContentPane ().add (contentPanel);
-	frame.setSize (600, 500);
+	frame.setSize (800, 700);
 	frame.setVisible (true);
 
     } // Constructor
