25f4a5eab1464f13658d9d8581406f21fb503baf
Nikki <46271636+nikkiguo@users.noreply.github.com>
2022-01-11 18:55:30 -0500

    Update Main.java

--- CA_NikkiGuo/Main.java
+++ CA_NikkiGuo/Main.java
@@ -1,16 +1,9 @@
-/*
- * Nikki Guo - Projectile Motion Simulator - MAIN.JAVA
- * This class's purpose to run the program, starting with the menu
- * This program simulates projectile motion given the user inputs (not a game)
-*/
-
 package CA_NikkiGuo;
 
 public class Main
 {
     public static void main (String[] args)
     {
-	// Runs SimulatorMenu.java
 	new SimulatorMenu ();
-    } // main method
-} // Main class
+    } 
+}
